e500c9100ed91fccace49c2189e87a28de758dac
hoseung2 <hoho331dcb@gmail.com>
2022-12-05 23:44:30 +0900

    dev setting

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -3,8 +3,8 @@ import styled from "styled-components";
 
 export default function Login() {
   const handleClick = async () => {
-    const client_id = "b10ac7cc459d474e961a6603c15da715";
-    const redirect_uri = "https://spotify-clone-api.netlify.app/";
+    const client_id = "2f315cc9f70b4daabf6c6e05ffdd21bd";
+    const redirect_uri = "http://localhost:3000/";
     const api_uri = "https://accounts.spotify.com/authorize";
     const scope = [
       "user-read-private",
